4cc8f7b9f8eafe9ff3ba5020031e2276a908801d
Devonltaylor <devonltaylor22@gmail.com>
2018-08-07 00:35:09 -0400

    Visual formatting

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +16,12 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Context context, ArrayList<Word> word) {
+    /**Resource ID for the background color for this list of words**/
+    private int mColorResourceId;
+
+    public WordAdapter(Context context, ArrayList<Word> word, int ColorResourceId) {
         super(context, 0, word);
+        mColorResourceId = ColorResourceId;
     }
 
     @NonNull
@@ -57,6 +62,13 @@ public class WordAdapter extends ArrayAdapter<Word> {
         } else {
                 imageView.setVisibility(View.GONE);
         }
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
